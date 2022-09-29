a7f80c8237dadc383bd82859ccf69fcc6db32d98
Missato Yamashita <rmissato@gmail.com>
2022-09-29 16:30:28 +0200

    Created functions to convert from C to F vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  const ftocFormula = (temperature - 32) * 5/9
+  const roundTo1Decimal = Math.round(ftocFormula * 10) / 10
+  return roundTo1Decimal
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  const ctofFormula = (temperature * 9/5) + 32
+  const roundTo1Decimal = Math.round(ctofFormula * 10) / 10
+  return roundTo1Decimal
 };
+
+console.log(ctof(73.2))
 
 // Do not edit below this line
 module.exports = {
