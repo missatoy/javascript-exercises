e369637d789b9d5b4ff642980b465a0752e170b9
Missato Yamashita <rmissato@gmail.com>
2022-10-13 12:17:55 +0200

    function to check if strings are palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  var regex = /[\W_]/g;
+  var lowRegStr = word.toLowerCase().replace(regex, '');
+  var reverseStr = lowRegStr.split('').reverse().join('');
+  return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
