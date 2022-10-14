8b83b76f73ae25ae4b6a6917eae547c3ad8beaae
Missato Yamashita <rmissato@gmail.com>
2022-10-14 12:26:01 +0200

    started fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let n1 = 0;
+  let n2 = 1;
+  let nextNum = 0;
 
+  for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextNum;
+  }
 };
 
 // Do not edit below this line
