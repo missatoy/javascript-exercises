09667d4ef1eebd6df1085b17baffb4db3854081f
Missato Yamashita <rmissato@gmail.com>
2022-10-14 14:20:27 +0200

    added condition to not accept negative numbers and turn string into integer

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -4,11 +4,17 @@ const fibonacci = function(num) {
   let nextNum = 0;
   let sequence = []
 
-  for (let i = 1; i <= num; i++) {
-    nextNum = a + b;
-    sequence.push(a)
-    a = b;
-    b = nextNum
+  if (Math.sign(num) === 1) {
+    for (let i = 1; i <= num; i++) {
+      nextNum = a + b;
+      sequence.push(a)
+      a = b;
+      b = nextNum
+    }
+  } else if (typeof num === "1") {
+    parseInt(num, 10)
+  } else {
+    return "OOPS"
   }
   return sequence[num - 1]
 };
